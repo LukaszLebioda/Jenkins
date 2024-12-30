7a02aa288abe834bc8dc78183f914ae9fb3d1667
Łukasz Lebioda <llebioda@novomatic-tech.com>
2024-12-30 11:34:12 +0100

    second commit

--- vanillajs.js
+++ vanillajs.js
@@ -1,14 +1,14 @@
-function print(txt) {
-	console.log(txt);
-}
+// function print(txt) {
+// 	console.log(txt);
+// }
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
+// let arr = [1, 2, 3, 4, 5, 6, 7];
 
-function showArray(arr, callback) {
-	for (let i = 0; i < arr.length; i++) {
-		const num = arr[i];
-		callback(num);
-	}
-}
+// function showArray(arr, callback) {
+// 	for (let i = 0; i < arr.length; i++) {
+// 		const num = arr[i];
+// 		callback(num);
+// 	}
+// }
 
-showArray(arr, print);
+// showArray(arr, print);
